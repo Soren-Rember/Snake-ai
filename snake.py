c60557df507cfcf8a91c6f62b71129da3e79da58
Soren-Rember <sneyts@gmail.com>
2026-04-29 18:01:25 +0200

    Add settings.txt and parse method

--- snake.py
+++ snake.py
@@ -1,18 +1,36 @@
 import pygame
+import time
+from enum import Enum
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+class Direction(Enum):
+    # Movement deltas
+    UP = (-1, 0)
+    DOWN = (+1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, +1)
 
 
 class Snake:
     def __init__(self, board_size) -> None:
         self.board = [[0] * board_size for _ in range(board_size)]
+        self.direction = Direction.UP
+        self.position = 
+
+    def parse_settings(self):
+        with open("settings.txt", "r") as settings:
+            lines = settings.readlines()
+        self.board_size = int(lines[0])
+        self.position = int(lines[1])
 
     def run(self):
         """Main loop"""
         running = True
         while running:
-            pass
+            time.sleep(0.75)
+            print("fiouf")
+            
 
 
 def main():
