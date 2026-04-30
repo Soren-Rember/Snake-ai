72ae261449c7b550ff3e70db56ea5436c393d417
Soren-Rember <sneyts@gmail.com>
2026-04-30 15:48:02 +0200

    Add the assets for snake

--- snake.py
+++ snake.py
@@ -43,6 +43,10 @@ class Snake:
         """Main loop"""
         running = True
         while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    
             keys = pygame.key.get_pressed()
             if keys[pygame.K_z]:
                 print("detected key z")
